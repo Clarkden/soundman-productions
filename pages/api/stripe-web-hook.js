68dfae24e5d9540a3c7e0e438e02fb402d2f0087
Clarkden <clarkh_@outlook.com>
2022-05-16 19:10:01 -0700

    hopefully done

--- pages/api/stripe-web-hook.js
+++ pages/api/stripe-web-hook.js
@@ -41,7 +41,7 @@ const handler = async (req, res) => {
 
             const purchaser = await db.collection('purchasers').findOne({name: session.metadata.name})
             if(purchaser != undefined){
-                await db.collection('purchasers').update(purchaser._id, {$push :{sounds: session.metadata.sound}})
+                await db.collection('purchasers').updateOne(purchaser._id, {$push :{sounds: session.metadata.sound}})
             }else{
                 await db.collection('purchasers').insertOne({name: session.metadata.name, sounds: [session.metadata.sound]})
             }
