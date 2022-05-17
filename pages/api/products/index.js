1513feaa94d4120eb31e5d3cfa3fe3bc5f18c838
Clarkden <clarkh_@outlook.com>
2022-05-16 22:32:22 -0700

    fixed loading error

--- pages/api/products/index.js
+++ pages/api/products/index.js
@@ -4,22 +4,19 @@ import clientPromise from "../../../lib/mongodb";
 
 export default async function handler(req, res) {
 
+    let client = await clientPromise;
+    const db = client.db("soundmanproductions");
 
-    const session = await getSession({ req })
-
-    if (session) {
-
-        let client = await clientPromise;
-        const db = client.db("soundmanproductions");
-
-        if (req.method === "POST") {
+    if (req.method === "POST") {
+        const session = await getSession({ req })
+        if (session) {
             const newProduct = await db.collection('songs').insertOne(req.body);
             res.status(200).json(newProduct);
-        }
-        else if (req.method === "GET") {
-            const allSongs = await db.collection('songs').find({}).toArray()
-            res.status(200).json(allSongs)
-        }
-    }else
-    res.status(403)
+        } else
+            res.status(403)
+    }
+    else if (req.method === "GET") {
+        const allSongs = await db.collection('songs').find({}).toArray()
+        res.status(200).json(allSongs)
+    }
 }
